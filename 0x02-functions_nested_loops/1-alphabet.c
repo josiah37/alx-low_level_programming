6ea85b72bb619c94765d53c01e7e88077bff8edd
= <=>
2023-10-11 13:39:52 +0000

    this has to work now or i will leave this task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,17 @@
 #include "main.h"
 /**
- * main- main prints the alphabet, in lowercase, followed by a new line.
+ * print_alphabet- print_alphabet prints the alphabet, in lowercase, followed by a new line.
  * description- this is the description for main returns a to z. alphabet.
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
 	char alphabet = 'a';
 
 	while (alphabet <= 'z')
 	{
 		_putchar(alphabet);
-		_putchar('\n');
 		alphabet++;
 	}
-	return (0);
+	_putchar('\n');
 }
