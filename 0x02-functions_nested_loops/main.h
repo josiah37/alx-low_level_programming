5b550b005c2c04d42158a8bc82569160337620f5
= <=>
2023-10-11 01:22:15 +0000

    finally i have finnished them all

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,28 @@
-/* function printString iterate over a string until it prints all characters */
+
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
+	
+	
+/*		what i have written below is something i made to answer task 0... i didnt know the above was alrady provided*/	
+	
+	
+	/* function printString iterate over a string until it prints all characters */
 void print_word(char string[])
 {
     int i = 0;
@@ -15,3 +39,5 @@ void printNewLine(void)
 {
     putchar('\n');
 }
+
+#endif
