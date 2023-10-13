8d38001029d537c6bfe157d835ee30922fc1c13b
= <=>
2023-10-13 15:02:28 +0000

    100-intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,9 @@
 #include <unistd.h>
 
 /**
- * main - Prints  ' "and that piece of art is useful" - Dora Korpar, 2015-10-19" ',
- *                followed by a new line, to standard error.
+ * main - Prints  the following
+ * ' "and that piece of art is useful" - Dora Korpar, 2015-10-19" ',
+ *             followed by a new line, to standard error.
  *
  * Return: Always 1.
  */
