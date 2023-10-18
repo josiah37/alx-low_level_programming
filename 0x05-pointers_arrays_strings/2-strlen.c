e791b09eeaa0cacb5d88f59c43a9fb4320fdd5e5
= <=>
2023-10-18 15:31:57 +0000

    fixing betty  errors on  2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,14 @@
 #include <string.h>
-int _strlen(char *s)
+/**
+ *_strlen- recives an argument to return the len of a string
+ *
+ * @s: The string whose length is to be calculatedCounts the length of a string
+ * Return: returns return len of a string to the main.
+ */
+int _strlen(char *s) /* _strlen - Counts the length of a string*/
 {
-        int count;
-        count = strlen (s);
-        return (count);
+	int count;
+
+	count = strlen(s);
+	return (count);
 }
