8fb0e498241eaff2c448b6786451d9a26d65d04b
= <=>
2023-10-28 23:48:25 +0000

    a commit for 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main- Entery point
+ * @argc: counting the parameters passed to main
+ * @argv: it is an array that prints the arguments passed to
+ * the main // after the compilation process
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
