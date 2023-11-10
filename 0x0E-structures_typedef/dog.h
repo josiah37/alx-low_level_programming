e9b9ac2202ad2893106ef00fef799b63f74f513e
= <=>
2023-11-10 13:44:29 +0000

    modify task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,30 +1,9 @@
 #ifndef DOG
 #define DOG
-
-/**
- * struct dog - dog class
- *
- * @name: Name of the dog
- *
- * @age: Age of the dog
- *
- * @owner: Owner of the dog
- */
-
 struct dog
 {
-	char *name;
+	char * name;
 	float age;
-	char *owner;
-};
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-
-typedef struct dog dog_t;
-
-dog_t *new_dog(char *name, float age, char *owner);
-
-void free_dog(dog_t *d);
-
+	char * owner;
+};	
 #endif
